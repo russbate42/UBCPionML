943d86e2c1caff341c60e98707f3ca49b675e85c
Maximilian Swiatlowski <mswiatlowski@triumf.ca>
2020-02-16 05:21:17 -0800

    Add plot utilities, and dependence on atlas mpl. lots of cluster studies updates.

---
+++ util/plot_util.py
@@ -0,0 +1,94 @@
+import matplotlib.font_manager
+import os
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+import atlas_mpl_style as ampl
+ampl.use_atlas_style()
+
+# set plotsytle choices here
+params = {'legend.fontsize': 13,
+          'axes.labelsize': 18}
+plt.rcParams.update(params)
+
+ampl.set_color_cycle('Paper')
+
+def histogramOverlay(frames, data, labels, xlabel, ylabel, figfile = '', 
+                        x_min = 0, x_max = 2200, xbins = 22, normed = True, y_log = False,
+                        atlas_x = -1, atlas_y = -1, simulation = False,
+                        textlist = []):
+    xbin = np.arange(x_min, x_max, (x_max - x_min) / xbins)
+
+    plt.cla()
+    plt.clf()
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
+
+    zorder_start = -1 * len(data) # hack to get axes on top
+    for i, datum in enumerate(data):
+        plt.hist(frames[i][datum], bins = xbin, normed = normed, 
+            alpha = 0.5, label=labels[i], zorder=zorder_start + i)
+    
+    plt.xlim(x_min, x_max)
+    if y_log:
+        plt.yscale('log')
+
+    ampl.set_xlabel(xlabel)
+    ampl.set_ylabel(ylabel)
+
+    if atlas_x >= 0 and atlas_y >= 0:
+        ampl.draw_atlas_label(atlas_x, atlas_y, simulation = simulation, fontsize = 18)
+
+    drawLabels(fig, atlas_x, atlas_y, simulation, textlist)
+    
+    fig.axes[0].zorder = len(data)+1 #hack to keep the tick marks up
+    plt.legend()
+    if figfile != '':
+        plt.savefig(figfile)
+    plt.show()
+
+def lineOverlay(xcenter, lines, labels, xlabel, ylabel, figfile = '',
+                    x_min = 0.1, x_max = 1000, x_log = True, y_min = 0, y_max = 2, y_log = False,
+                    extra_lines = [],
+                    atlas_x=-1, atlas_y=-1, simulation=False,
+                    textlist=[]):
+    plt.cla()
+    plt.clf()
+
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
+    for extra_line in extra_lines:
+        plt.plot(extra_line[0], extra_line[1], linestyle='--', color='black')
+
+    for i, line in enumerate(lines):
+        plt.plot(xcenter, line, label = labels[i])
+
+    if x_log:
+        plt.xscale('log')
+    if y_log:
+        plt.yscale('log')
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+    ampl.set_xlabel(xlabel)
+    ampl.set_ylabel(ylabel)
+
+    drawLabels(fig, atlas_x, atlas_y, simulation, textlist)
+
+    plt.legend()
+    if figfile != '':
+        plt.savefig(figfile)
+    plt.show()
+
+
+def drawLabels(fig, atlas_x=-1, atlas_y=-1, simulation=False,
+               textlist=[]):
+    if atlas_x >= 0 and atlas_y >= 0:
+        ampl.draw_atlas_label(atlas_x, atlas_y, simulation=simulation, fontsize=18)
+
+    for textdict in textlist:
+        fig.axes[0].text(
+            textdict['x'], textdict['y'], textdict['text'], 
+            transform=fig.axes[0].transAxes, fontsize=18)
--- util/resolution_util.py
+++ util/resolution_util.py
@@ -7,8 +7,12 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 import scipy.stats as stats
 
+from . import plot_util as pu
 
-def responsePlot(x, y, name='', statistic='median'):
+
+def responsePlot(x, y, figfile='', statistic='median',
+                 atlas_x=-1, atlas_y=-1, simulation=False,
+                 textlist=[]):
     xbin = [10**exp for exp in np.arange(-1.0, 3.1, 0.1)]
     ybin = np.arange(0., 3.1, 0.1)
     xcenter = [(xbin[i] + xbin[i+1]) / 2 for i in range(len(xbin)-1)]
@@ -19,17 +23,22 @@ def responsePlot(x, y, name='', statistic='median'):
     plt.clf()
     fig = plt.figure()
     fig.patch.set_facecolor('white')
-    plt.hist2d(x, y, bins=[xbin, ybin], norm=LogNorm())
-    plt.plot([0.1, 1000], [1, 1], linestyle='--')
-    plt.plot(xcenter, profileXMed)
+    plt.hist2d(x, y, bins=[xbin, ybin], norm=LogNorm(),zorder = -1)
+    plt.plot([0.1, 1000], [1, 1], linestyle='--', color='black')
+    plt.plot(xcenter, profileXMed, color='red')
     plt.xscale('log')
     plt.ylim(0, 3)
-    plt.xlabel('Cluster Calib Hits')
-    plt.ylabel('Cluster Energy over Calib Hits')
-    plt.colorbar()
+    pu.ampl.set_xlabel('Cluster Calib Hits')
+    pu.ampl.set_ylabel('Cluster Energy / Calib Hits')
+    # ampl.set_zlabel('Clusters')
+    cb = plt.colorbar()
+    cb.ax.set_ylabel('Clusters')
     # plt.legend()
-    if name != '':
-        plt.savefig(name+'.pdf')
+
+    pu.drawLabels(fig, atlas_x, atlas_y, simulation, textlist)
+
+    if figfile != '':
+        plt.savefig(figfile)
     plt.show()
 
     return xcenter, profileXMed
@@ -43,12 +52,14 @@ def stdOverMean(x):
 def iqrOverMed(x):
     # get the IQR via the percentile function
     # 84 is median + 1 sigma, 16 is median - 1 sigma
-    q68, q16 = np.percentile(x, [84, 16])
-    iqr = q68 - q16
+    q84, q16 = np.percentile(x, [84, 16])
+    iqr = q84 - q16
     med = np.median(x)
     return iqr / med
 
-def resolutionPlot(x, y, name='', statistic='std'):
+def resolutionPlot(x, y, figfile='', statistic='std',
+                   atlas_x=-1, atlas_y=-1, simulation=False,
+                   textlist=[]):
     xbin = [10**exp for exp in  np.arange(-1.0, 3.1, 0.1)]
     xcenter = [(xbin[i] + xbin[i+1]) / 2 for i in range(len(xbin)-1)]
     if statistic == 'std': # or any other baseline one?
@@ -63,11 +74,15 @@ def resolutionPlot(x, y, name='', statistic='std'):
     fig.patch.set_facecolor('white')
     plt.plot(xcenter, resolution)
     plt.xscale('log')
+    plt.xlim(0.1, 1000)
     plt.ylim(0,2)
-    plt.xlabel('Cluster Calib Hits')
-    plt.ylabel('Cluster Energy RMS over Mean')
-    if name != '':
-        plt.savefig(name+'.pdf')
+    pu.ampl.set_xlabel('Cluster Calib Hits')
+    pu.ampl.set_ylabel('Cluster Energy IQR over Median')
+
+    pu.drawLabels(fig, atlas_x, atlas_y, simulation, textlist)
+
+    if figfile != '':
+        plt.savefig(figfile)
     plt.show()
 
     return xcenter, resolution

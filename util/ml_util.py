d22908d86803c5342a3ce33216d38b5ee3c088aa
Maximilian Swiatlowski <mswiatlowski@triumf.ca>
2020-02-21 09:51:52 -0800

    more work on regression: using all EMB1 info

--- util/ml_util.py
+++ util/ml_util.py
@@ -1,5 +1,46 @@
 import numpy as np  
 from sklearn.model_selection import ShuffleSplit
+from sklearn.preprocessing import StandardScaler
+
+#define a dict for cell meta data
+cell_meta = {
+    'EMB1': {
+        'cell_size_phi': 0.098,
+        'cell_size_eta': 0.0031,
+        'len_phi': 4,
+        'len_eta': 128
+    },
+    'EMB2': {
+        'cell_size_phi': 0.0245,
+        'cell_size_eta': 0.025,
+        'len_phi': 16,
+        'len_eta': 16
+    },
+    'EMB3': {
+        'cell_size_phi': 0.0245,
+        'cell_size_eta': 0.05,
+        'len_phi': 16,
+        'len_eta': 8
+    },
+    'TileBar0': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.1,
+        'len_phi': 4,
+        'len_eta': 4
+    },
+    'TileBar1': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.1,
+        'len_phi': 4,
+        'len_eta': 4
+    },
+    'TileBar2': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.2,
+        'len_phi': 4,
+        'len_eta': 2
+    },
+}
 
 def splitFrameTVT(frame, trainlabel='train', trainfrac = 0.8, testlabel='test', testfrac = 0.2, vallabel='val'):
 
@@ -21,3 +62,34 @@ def splitFrameTVT(frame, trainlabel='train', trainfrac = 0.8, testlabel='test', 
     frame[testlabel]  = frame.index.isin(test_index)
     frame[vallabel]   = frame.index.isin(val_index)
 
+def setupCells(tree, layer, nrows = -1, flatten=True):
+    array = tree.array(layer)
+    if nrows > 0:
+        array = array[:nrows]
+    num_pixels = cell_meta[layer]['len_phi'] * cell_meta[layer]['len_eta']
+    if flatten:
+        array = array.reshape(len(array), num_pixels)
+    else:
+        print('Not implemented yet! do not do this')
+        return
+    
+    return array
+
+def standardCells(array, layer, nrows = -1):
+    if nrows > 0:
+        working_array = array[:nrows]
+    else:
+        working_array = array
+
+    scaler = StandardScaler()
+    num_pixels = cell_meta[layer]['len_phi'] * cell_meta[layer]['len_eta']
+
+    num_clusters = len(working_array)
+
+    flat_array = np.array(working_array.reshape(num_clusters * num_pixels, 1))
+
+
+    scaled = scaler.fit_transform(flat_array)
+
+    reshaped = scaled.reshape(num_clusters, num_pixels)
+    return reshaped, scaler

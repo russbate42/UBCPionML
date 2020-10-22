2f7f1e189e8f843606d94b81e70a24a545cdc3f1
Jan Offermann <offermannjan@yahoo.com>
2020-10-21 21:23:08 -0400

    adding setup script

---
+++ setup/setup.sh
@@ -0,0 +1,12 @@
+# create the conda environment "ml4p"
+conda env create --file conda/ml4p.yml
+
+# check for latex, needed for atlas_mpl_style
+if ! command -v latex &> /dev/null
+then
+    echo "Did not find latex. Installing tex-live 2020."
+    tar -xf tex/install-tl-unx.tar.gz
+    ./install-tl-20201021/install-tl --profile=tex/texlive.profile
+    export PATH="/usr/local/texlive/2020/bin/x86_64-linux:$PATH"
+    exit
+fi

6ed42f963635be854c86d443f96a18812f6a3199
Jan Offermann <offermannjan@yahoo.com>
2020-10-23 18:59:29 -0400

    adding plot and model folders for classification

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,4 +1,5 @@
 # create the conda environment "ml4p"
+echo ""
 conda env create --file conda/ml4p.yml
 
 # check for latex, needed for atlas_mpl_style
@@ -8,5 +9,8 @@ then
     tar -xf tex/install-tl-unx.tar.gz
     ./install-tl-20201021/install-tl --profile=tex/texlive.profile
     export PATH="/usr/local/texlive/2020/bin/x86_64-linux:$PATH"
+    echo "Finished installing tex-live 2020"
     exit
 fi
+
+

e3b526f273d32c948f3d22a23257ef7aa4f1dfdc
[Jan Offermann] <[offermannjan@yahoo.com]>
2020-11-03 23:09:15 +0000

    network scores added after event clustering

--- util/qol_util.py
+++ util/qol_util.py
@@ -1,5 +1,7 @@
 # Just some simple, quality-of-life functions. Nothing very fancy.
 
+import numpy as np
+
 # Print iterations progress.
 # Adapted from https://stackoverflow.com/a/34325723.
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
@@ -33,3 +35,33 @@ def printProgressBarColor (iteration, total, prefix = '', suffix = '', decimals 
     fill = fill_prefix + fill + fill_suffix
     printProgressBar(iteration, total, prefix = prefix, suffix = suffix, decimals = decimals, length = length, fill = fill, printEnd = printEnd)
     return
+
+
+# Some ROOT/numpy stuff
+# Converting from ROOT type names to leaflist decorators.
+# Vector decorator will not work, but gives a sensible string
+# telling us the depth (how many vectors).
+def RTypeConversion(type_string):
+    if(type_string == 'Short_t' or type_string == 'short'):    return 'S'
+    elif(type_string == 'Int_t' or type_string == 'int'):    return 'I'
+    elif(type_string == 'Float_t' or type_string == 'float'):  return 'F'
+    elif(type_string == 'Double_t' or type_string == 'double'): return 'D'
+    elif('vector' in type_string): # special case
+#         type_substring = '<'.join(type_string.split('<')[1:])
+#         type_substring = '>'.join(type_substring.split('>')[:-1])
+#         type_substring = RTypeConversion(type_substring)
+#         return 'v_' + type_substring
+        return type_string
+    else: return '?'
+
+def GetShape(shape_string):
+    dims = shape_string.replace('[',' ').replace(']', ' ').split()
+    return tuple([int(x) for x in dims])
+
+def RType2NType(type_string):
+    if(type_string == 'S'):   return np.dtype('i2')
+    elif(type_string == 'I'): return np.dtype('i4')
+    elif(type_string == 'L'): return np.dtype('i8')
+    elif(type_string == 'F'): return np.dtype('f4')
+    elif(type_string == 'D'): return np.dtype('f8')
+    else: raise ValueError('Input not understood.')

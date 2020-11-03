32f2f5beb269aeec0b41d946464f7b31f72d3a57
[Jan Offermann] <[offermannjan@yahoo.com]>
2020-11-03 22:01:53 +0000

    splitting up workflow, rewriting to add network scores after event reconstruction

---
+++ jets/util/image_util.py
@@ -0,0 +1,17 @@
+import uproot as ur
+import numpy as np
+
+# Returns a flattened list of calo images.
+# Ordering is consistent with event/topo-cluster ordering in input.
+def setupCells(calo_images, layer, nevents = -1):
+        
+    # 1st dim is number of clusters (VARIABLE)
+    # 2nd dim is eta (or phi?) (CONSTANT)
+    # 3rd dim is phi (or eta?) (CONSTANT)
+    if(nevents > 0): image_set = calo_images[layer][:nevents].flatten()
+    else: image_set = calo_images[layer].flatten() # flattened list of 2D images
+    # get number of pixels from shape of initial entry
+    num_pixels = image_set.shape[-1] * image_set.shape[-2]
+    image_set = image_set.reshape(image_set.shape[0],num_pixels)
+    return image_set
+    

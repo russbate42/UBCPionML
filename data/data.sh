ea5a689190b12a958edcef0edb615a570a914466
Jan Offermann <offermannjan@yahoo.com>
2020-11-20 20:29:44 +0000

    updating data download

--- data/data.sh
+++ data/data.sh
@@ -1,3 +1,14 @@
-# Getting our trial data from some fixed location (for now)
+# Getting our trial data from some fixed locations (for now.)
 user=jaofferm
-scp ${user}@lxplus.cern.ch:/eos/user/m/mswiatlo/images/v7/*.root ./
+
+type=$1 #this will indicate the kind of dataset we want to download
+
+if [[ "$type" == "jet" ]]
+then
+    scp -r ${user}@lxplus.cern.ch:/eos/user/j/jaofferm/atlas-calo-ml/data/jet/* ./
+elif [[ "$type" == "pion" ]]
+then
+    scp ${user}@lxplus.cern.ch:/eos/user/m/mswiatlo/images/v7/*.root ./
+else
+    echo "Type ${type} not understood."
+fi

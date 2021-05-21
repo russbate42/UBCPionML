cd1e1ed3b72301f049398702cdef3aeda713f917
Maximilian Swiatlowski <mswiatlowski@triumf.ca>
2021-05-21 17:56:40 +0200

    more graph utils

--- util/graph_util.py
+++ util/graph_util.py
@@ -3,6 +3,9 @@ import awkward as ak
 import numpy as np
 
 
+#given a branchname, a tree from uproot, and a padLength...
+#return a flattened numpy array that flattens away the event index and pads cels to padLength
+#if there's no cell, add a 0 value
 def loadBranchFlat(branchName, tree, padLength):
     branchInfo = tree[branchName].array()
 
@@ -20,3 +23,37 @@ def loadBranchFlat(branchName, tree, padLength):
     branchFlatNumpy[-1] = 0
 
     return branchFlatNumpy
+
+# A quick implemention of Dilia's idea for converting the geoTree into a dict
+def loadGraphDictionary(tree):
+    # make a global dict. this will be keyed by strings for which info you want
+    globalDict = {}
+
+    #get the information
+    arrays = tree.arrays()
+    keys = tree.keys()
+    for key in keys:
+        #skip geoID-- that's our new key
+        if key == 'cell_geo_ID': 
+            continue
+        branchDict = {}
+        # loop over the entries of the GEOID array (maybe this should be hte outer array? eh.)
+        # [0] is used here and below to remove a false index
+        for iter, ID in enumerate(arrays['cell_geo_ID'][0]):
+            #the key is the ID, the value is whatever we iter over
+            branchDict[ID] = arrays[key][0][iter] 
+        
+        #make a saftey for 0
+        branchDict[0] = 0
+
+        globalDict[key] = branchDict
+
+    return globalDict
+
+
+# given a list of Cell IDs and a target from the geometry tree specified in geoString
+# (and given the globalDict containing the ID->info mappings)
+# return a conversion of the cell IDs to whatever is requested
+def convertIDToGeo(cellID, geoString, globalDict):
+    # MAGIC https://stackoverflow.com/questions/16992713/translate-every-element-in-numpy-array-according-to-key
+    return np.vectorize(globalDict[geoString].get)(cellID)

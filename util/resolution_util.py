d204134feaa52df4fd788d0f26363438fcef3344
russbate42 <r.bate@hotmail.com>
2021-06-11 14:30:11 -0700

    better normalization on clusters regression, working poorly still.

--- util/resolution_util.py
+++ util/resolution_util.py
@@ -53,7 +53,7 @@ def stdOverMean(x):
 def iqrOverMed(x):
     # get the IQR via the percentile function
     # 84 is median + 1 sigma, 16 is median - 1 sigma
-    q84, q16 = np.percentile(x, [75, 25])
+    q84, q16 = np.percentile(x, [84, 16])
     iqr = q84 - q16
     med = np.median(x)
     return iqr / med

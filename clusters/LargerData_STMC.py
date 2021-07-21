9944344cdcdc3149383937a16c0406aa7e6626cd
russbate42 <r.bate@hotmail.com>
2021-07-21 10:55:50 -0700

    Single track single cluster, single track multiple cluster have both been updated to use memory mapped arrays in order to circumvent concatenation time delay. More accurate measure of time. Three targets have been added for both data sets for truthPartE, trackPt, cluster_ENG_CALIB_TOT or sum of cluster_ENG_CALIB_TOT.

--- clusters/LargerData_STMC.py
+++ clusters/LargerData_STMC.py
@@ -26,26 +26,33 @@ def DeltaR(coords, ref):
     DeltaCoords[gt_pi_mask] = DeltaCoords[gt_pi_mask] - 2*np.pi
     return np.sqrt(DeltaCoords[:,0]**2 + DeltaCoords[:,1]**2)
 
-def find_max_dim(events, event_dict):
-    ''' This function is designed to return the sizes of a numpy array such that we are efficient
-    with zero padding. Notes: we add six to the maximum cluster number such that we have room for track info.
-    Inputs:
-        events: filtered list of events to choose from in an Nx3 format for event, track, cluster index 
-        event_tree: the event tree dictionary
-    Returns:
-        3-tuple consisting of (number of events, maximum cluster_size, 6), 6 because of how we have structured
-        the X data format in energyFlow to be Energy, Eta, Phi, rPerp, track_flag, sampling_layer
-    '''
+def find_max_dim_tuple(events, event_dict):
     nEvents = len(events)
     max_clust = 0
+    
     for i in range(nEvents):
-        evt = events[i,0]
-        clust_idx = events[i,2]
-        num_in_clust = len(event_dict['cluster_cell_ID'][evt][clust_idx])
-        if num_in_clust > max_clust:
-            max_clust = num_in_clust
+        event = events[i,0]
+        track_nums = events[i,1]
+        clust_nums = events[i,2]
+        
+        clust_num_total = 0
+        # set this to six for now to handle single track events, change later
+        track_num_total = 6
+        
+        # Check if there are clusters, None type object may be associated with it
+        if clust_nums is not None:
+            # Search through cluster indices
+            for clst_idx in clust_nums:
+                nInClust = len(event_dict['cluster_cell_ID'][event][clst_idx])
+                # add the number in each cluster to the total
+                clust_num_total += nInClust
 
-    return (nEvents, max_clust+6, 6)
+        total_size = clust_num_total + track_num_total
+        if total_size > max_clust:
+            max_clust = total_size
+    
+    # 6 for energy, eta, phi, rperp, track flag, sample layer
+    return (nEvents, max_clust, 6)
 
 def dict_from_tree(tree, branches=None, np_branches=None):
     ''' Loads branches as default awkward arrays and np_branches as numpy arrays. '''
@@ -128,9 +135,9 @@ events_prefix = '/fast_scratch/atlas_images/v01-45/pipm/'
 firstArray = True
 
 ## MEMORY MAPPED ARRAY ALLOCATION ##
-X_STSC_large = np.lib.format.open_memmap('/data/rbate/X_STSC_large.npy', mode='w+', dtype=np.float64,
+X_large = np.lib.format.open_memmap('/data/rbate/X_large.npy', mode='w+', dtype=np.float64,
                        shape=(2000000,2000,6), fortran_order=False, version=None)
-Y_STSC_large = np.lib.format.open_memmap('/data/rbate/Y_STSC_large.npy', mode='w+', dtype=np.float64,
+Y_large = np.lib.format.open_memmap('/data/rbate/Y_large.npy', mode='w+', dtype=np.float64,
                        shape=(2000000,3), fortran_order=False, version=None)
 
 k = 1 # tally used to keep track of file number
@@ -194,9 +201,12 @@ for currFile in fileNames:
 
     for evt in filtered_event:
 
-        # pull cluster number, don't need zero index as it's loaded as a np array
+        # pull cluster number, don't need zero as it's loaded as a np array
         nClust = event_dict["nCluster"][evt]
         cluster_idx = np.arange(nClust)
+
+        # Notes: this will need to handle more complex scenarios in the future for tracks with
+        # no clusters
 
         ## DELTA R ##
         # pull coordinates of tracks and clusters from event
@@ -208,20 +218,12 @@ for currFile in fileNames:
 
         _DeltaR = DeltaR(clusterCoords, trackCoords)
         DeltaR_mask = _DeltaR < .2
-        
-        ## ENERGY SELECTION
-        clusterEs = event_dict["cluster_E"][evt].to_numpy()
-        tot_clustEs = np.sum(clusterEs)
-        Energy_mask = clusterEs/tot_clustEs >= .9
-        
-        ## DELTA R AND ENERGY
-        combined_mask = np.logical_and(DeltaR_mask, Energy_mask)
-        clst_idx_matched = np.argmax(combined_mask)
+        matched_clusters = cluster_idx[DeltaR_mask]
 
         ## CREATE LIST ##
         # Note: currently do not have track only events. Do this in the future    
-        if np.count_nonzero(combined_mask) > 0:
-            event_indices.append((evt, 0, clst_idx_matched))
+        if np.count_nonzero(DeltaR_mask) > 0:
+            event_indices.append((evt, 0, matched_clusters))
     
     event_indices = np.array(event_indices, dtype=np.object_)
     t1 = t.time()
@@ -231,7 +233,7 @@ for currFile in fileNames:
     ## DIMENSIONS OF X ARRAY ##
     #=========================#
     t0 = t.time()
-    max_dims = find_max_dim(event_indices, event_dict)
+    max_dims = find_max_dim_tuple(event_indices, event_dict)
     evt_tot = max_dims[0]
     tot_nEvts += max_dims[0]
     # keep track of the largest point cloud to use for saving later
@@ -239,10 +241,10 @@ for currFile in fileNames:
         max_nPoints = max_dims[1]
     
     # Create arrays
-    Y_new = np.zeros((max_dims[0], 3))
+    Y_new = np.zeros((max_dims[0],3))
     X_new = np.zeros(max_dims)
     t1 = t.time()
-    find_create_max_dims_time = t1 - t0
+    find_create_max_dims_time = t1 - t0    
     
     #===================#
     ## FILL IN ENTRIES ##==============================================================
@@ -252,45 +254,62 @@ for currFile in fileNames:
         # pull all relevant indices
         evt = event_indices[i,0]
         track_idx = event_indices[i,1]
-        # returns single cluster index
-        cluster_idx = event_indices[i,2]
+        # recall this now returns an array
+        cluster_nums = event_indices[i,2]
 
         ##############
         ## CLUSTERS ##
         ##############
-        # find center of clusters
-        cluster_Eta = event_dict['cluster_Eta'][evt][cluster_idx]
-        cluster_Phi = event_dict['cluster_Phi'][evt][cluster_idx]
+        # set up to have no clusters, further this with setting up the same thing for tracks
+        target_ENG_CALIB_TOT = -1
+        if cluster_nums is not None:
 
-        # cluster data
-        cluster_cell_ID = event_dict['cluster_cell_ID'][evt][cluster_idx].to_numpy()
-        nInClust = len(cluster_cell_ID)
-        cluster_cell_E = event_dict['cluster_cell_E'][evt][cluster_idx].to_numpy()
-        cell_indices = find_index_1D(cluster_cell_ID, cell_ID_dict)
+            # find averaged center of clusters
+            cluster_Eta = event_dict['cluster_Eta'][evt].to_numpy()
+            cluster_Phi = event_dict['cluster_Phi'][evt].to_numpy()
+            av_Eta = np.mean(cluster_Eta)
+            av_Phi = np.mean(cluster_Phi)
 
-        cluster_cell_Eta = geo_dict['cell_geo_eta'][cell_indices]
-        cluster_cell_Phi = geo_dict['cell_geo_phi'][cell_indices]
-        cluster_cell_rPerp = geo_dict['cell_geo_rPerp'][cell_indices]
-        cluster_cell_sampling = geo_dict['cell_geo_sampling'][cell_indices]
+            nClust_current_total = 0
+            target_ENG_CALIB_TOT = 0
+            for c in cluster_nums:            
+                # cluster data
+                target_ENG_CALIB_TOT += event_dict['cluster_ENG_CALIB_TOT'][evt][c]
+                cluster_cell_ID = event_dict['cluster_cell_ID'][evt][c].to_numpy()
+                nInClust = len(cluster_cell_ID)
+                cluster_cell_E = event_dict['cluster_cell_E'][evt][c].to_numpy()            
+                cell_indices = find_index_1D(cluster_cell_ID, cell_ID_dict)
 
-        # input all the data
-        X_new[i,:nInClust,0] = cluster_cell_E
-        # Normalize to cluster center
-        X_new[i,:nInClust,1] = cluster_cell_Eta - cluster_Eta
-        X_new[i,:nInClust,2] = cluster_cell_Phi - cluster_Phi
-        X_new[i,:nInClust,3] = cluster_cell_rPerp
-        X_new[i,:nInClust,5] = cluster_cell_sampling
+                cluster_cell_Eta = geo_dict['cell_geo_eta'][cell_indices]
+                cluster_cell_Phi = geo_dict['cell_geo_phi'][cell_indices]
+                cluster_cell_rPerp = geo_dict['cell_geo_rPerp'][cell_indices]
+                cluster_cell_sampling = geo_dict['cell_geo_sampling'][cell_indices]
+
+                # input all the data
+                # note here we leave the fourth entry zeros (zero for flag!!!)
+                low = nClust_current_total
+                high = low + nInClust
+                X_new[i,low:high,0] = cluster_cell_E
+                # Normalize to average cluster centers
+                X_new[i,low:high,1] = cluster_cell_Eta - av_Eta
+                X_new[i,low:high,2] = cluster_cell_Phi - av_Phi
+                X_new[i,low:high,3] = cluster_cell_rPerp
+                X_new[i,low:high,5] = cluster_cell_sampling
+
+                nClust_current_total += nInClust
 
         #####################
         ## TARGET ENERGIES ##
         #####################
+        # this should be flattened or loaded as np array instead of zeroth index in future
         Y_new[i,0] = event_dict['truthPartE'][evt][0]
         Y_new[i,1] = event_dict['truthPartPt'][evt][track_idx]
-        Y_new[i,2] = event_dict['cluster_ENG_CALIB_TOT'][evt][cluster_idx]
+        Y_new[i,2] = target_ENG_CALIB_TOT
 
         ############
         ## TRACKS ##
         ############
+
         trackP = event_dict['trackP'][evt][track_idx]
 
         track_arr = np.zeros((6,6))
@@ -315,11 +334,11 @@ for currFile in fileNames:
         track_eta_null_mask = np.abs(track_arr[:,1]) > 4.9
         track_phi_null_mask = np.abs(track_arr[:,2]) >= np.pi
         track_flag_null = np.logical_or(track_eta_null_mask, track_phi_null_mask)
-
-        # Normalize track information!
-        track_arr[:,1] = track_arr[:,1] - cluster_Eta
-        track_arr[:,2] = track_arr[:,2] - cluster_Phi
         
+        # Normalize track information!
+        track_arr[:,1] = track_arr[:,1] - av_Eta
+        track_arr[:,2] = track_arr[:,2] - av_Phi
+
         # where the flag is set to null, set values of energy and calo layer to zero
         if np.any(track_flag_null):
             # number for which to spread the energy out over
@@ -329,10 +348,10 @@ for currFile in fileNames:
             track_arr[np.invert(track_flag_null),0] = trackP/p_nums
         # otherwise fill in pt/6 for all
         else:
-            track_arr[:,0] = trackP/6
+            track_arr[:,0] = trackP/6      
 
-        # Save Track Information
-        X_new[i,nInClust:nInClust+6,:6] = track_arr
+        # Save track information
+        X_new[i,high:high+6,0:6] = track_arr
 
     #####################################################
     t1 = t.time()
@@ -344,13 +363,13 @@ for currFile in fileNames:
     t0 = t.time()
     # Write to X
     old_tot = tot_nEvts - max_dims[0]
-    X_STSC_large[old_tot:tot_nEvts, :max_dims[1], :6] = np.ndarray.copy(X_new)
+    X_large[old_tot:tot_nEvts, :max_dims[1], :6] = np.ndarray.copy(X_new)
     # pad the remainder with zeros (just to be sure)
     fill_shape = (tot_nEvts - old_tot, 2000 - max_dims[1], 6)
-    X_STSC_large[old_tot:tot_nEvts, max_dims[1]:2000, :] = np.zeros(fill_shape)
+    X_large[old_tot:tot_nEvts, max_dims[1]:2000, :6] = np.zeros(fill_shape)
     
     # Write to Y
-    Y_STSC_large[old_tot:tot_nEvts,:] = np.ndarray.copy(Y_new)
+    Y_large[old_tot:tot_nEvts] = np.ndarray.copy(Y_new)
         
     t1 = t.time()
     time_to_memmap = t1-t0
@@ -371,8 +390,12 @@ for currFile in fileNames:
     print()
     
 t0 = t.time()
-np.savez('/data/rbate/XY_STSC_allFiles', X_STSC_large, Y_STSC_large)
+np.savez('/data/rbate/XY_STMC_allFiles', X_large[:tot_nEvts, :max_nPoints, :], Y_large[:tot_nEvts,:])
 t1 = t.time()
 print()
 print('Time to save file: '+str(t1-t0)+' (s)')
 print()
+        
+    
+
+

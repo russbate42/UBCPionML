f1c73749abf771f8a18f5359bdd3fda2dca62347
russbate42 <r.bate@hotmail.com>
2021-10-01 15:57:58 -0700

    Working data set script for Rho. Cleaned up file space and moved all regressions to the proper folder. Started Preliminary notebooks for semantic segmentation.

--- clusters/LargerData_Rho.py
+++ clusters/LargerData_Rho.py
@@ -32,14 +32,12 @@ from deep_set_util import track_branches, event_branches, ak_event_branches, np_
 #====================
 # user.angerami.24559744.OutputStream._000001.root
 # Number of files
-Nfile = 40
+Nfile = 42
 fileNames = []
-file_prefix = 'user.angerami.24409109.OutputStream._000'
+file_prefix = 'user.akong.26471083.OutputStream._000'
 for i in range(1,Nfile+1):
     endstring = f'{i:03}'
     fileNames.append(file_prefix + endstring + '.root')
-
-print(fileNames)
 
 
 #====================
@@ -49,7 +47,7 @@ print(fileNames)
 ## GEOMETRY DICTIONARY ##
 geo_file = ur.open('/fast_scratch/atlas_images/v01-45/cell_geo.root')
 CellGeo_tree = geo_file["CellGeo"]
-geo_dict = dict_from_tree(tree=CellGeo_tree, branches=None, np_branches=geo_branches)
+geo_dict = dsu.dict_from_tree(tree=CellGeo_tree, branches=None, np_branches=geo_branches)
 
 # cell geometry data
 cell_geo_ID = geo_dict['cell_geo_ID']
@@ -60,13 +58,214 @@ layer_rPerp = np.array([1540., 1733., 1930., 2450., 3010., 3630.])
 track_sample_layer = np.array([1,2,3,12,13,14])
 
 # for event dictionary
-events_prefix = '/fast_scratch/atlas_images/v01-45/rho/'
+events_prefix = '/data/atlas/akong/singlerho-percell/'
 
 # Use this to compare with the dimensionality of new events
 firstArray = True
 
-## MEMORY MAPPED ARRAY ALLOCATION ##
-# XR_large = np.lib.format.open_memmap('/data/rbate/XR_large.npy', mode='w+', dtype=np.float64,
-#                        shape=(1700000,1500,6), fortran_order=False, version=None)
-# YR_large = np.lib.format.open_memmap('/data/rbate/YR_large.npy', mode='w+', dtype=np.float64,
-#                        shape=(1700000,3), fortran_order=False, version=None)
+# MEMORY MAPPED ARRAY ALLOCATION ##
+XR_large = np.lib.format.open_memmap('/data/atlas/rbate/XR_large.npy', mode='w+', dtype=np.float64,
+                       shape=(200000,1700,4), fortran_order=False, version=None)
+YR_large = np.lib.format.open_memmap('/data/atlas/rbate/YR_large.npy', mode='w+', dtype=np.float64,
+                       shape=(200000,1700,2), fortran_order=False, version=None)
+
+k = 1 # tally used to keep track of file number
+tot_nEvts = 0 # used for keeping track of total number of events
+max_nPoints = 0 # Keep track of the largest point cloud overall
+biggest_pCloud = 0 # used for keeping track of the largest 'point cloud' per root file
+t_tot = 0 # total time
+
+for currFile in fileNames:
+    
+    # Check for file, a few are missing
+    if not os.path.isfile(events_prefix+currFile):
+        print()
+        print('File '+events_prefix+currFile+' not found..')
+        print()
+        k += 1
+        continue
+    
+    else:
+        print()
+        print('Working on File: '+str(currFile)+' - '+str(k)+'/'+str(Nfile))
+        k += 1
+
+    t0 = t.time()
+    ## EVENT DICTIONARY ##
+    event = ur.open(events_prefix+currFile)
+    event_tree = event["EventTree"]
+    event_dict = dsu.dict_from_tree(tree=event_tree, branches=ak_event_branches, np_branches=np_event_branches)
+    
+    ## TRACK DICTIONARY ##
+    track_dict = dsu.dict_from_tree(tree=event_tree, branches=track_branches)
+    
+    #===================
+    # APPLY CUTS =======
+    #===================
+    ''' This section combines the previous find_max_dim_tuple function seeing as we are already searching
+    for the size of the point cloud anyways. Simultaneous cluster index creation and mem map size. '''
+    event_indices = []
+    t0 = t.time()
+    # create ordered list of events to use for index slicing
+    nEvts_in_file = len(event_dict['eventNumber'])
+    
+    # loop through all clusters
+    for i in range(nEvts_in_file):
+        
+        nClust = event_dict['nCluster'][i]
+        cluster_eta_cut = np.zeros(nClust,dtype=np.bool_)
+        cluster_ENG_cut = np.zeros(nClust,dtype=np.bool_)
+        min_points_cut = np.zeros(nClust,dtype=np.bool_)
+        this_pCloud = 0
+        
+        for j in range(nClust):
+            
+            cluster_ENG = event_dict['cluster_E'][i][j]
+            if cluster_ENG > 50:
+                cluster_ENG_cut[j] = True
+            
+            if event_dict['cluster_Eta'][i][j] < .7:
+                cluster_eta_cut[j] = True
+                this_pCloud += len(event_dict['cluster_cell_ID'][i][j])
+        
+        if this_pCloud > 10:
+            min_points_cut.fill(True)
+        
+        ## CHECK CUTS AND ADD TO LIST
+        # note numpy needs 0 index for where because it returns a tuple
+        clusters_post_cut = np.logical_and(cluster_eta_cut, cluster_ENG_cut, min_points_cut)
+        if np.any(clusters_post_cut):
+            clusters_idx = np.where(clusters_post_cut)[0]
+            event_indices.append((i, clusters_idx))
+        
+        if this_pCloud > biggest_pCloud:
+            biggest_pCloud = this_pCloud
+
+    nEvts = len(event_indices)
+    print('Number of events after filter: '+str(nEvts))
+    event_indices = np.array(event_indices, dtype=np.object_)
+    t1 = t.time()
+    event_cuts_time = t1 - t0
+    
+    
+    #=======================#
+    ## CREATE X, Y ARRAYS  ##
+    #=======================#
+    max_dims = (nEvts, biggest_pCloud, 4)
+    tot_nEvts += nEvts
+    if biggest_pCloud > max_nPoints:
+        max_nPoints = biggest_pCloud
+        
+    
+    t0 = t.time()
+    Y_new = np.zeros((nEvts,biggest_pCloud,2))
+    X_new = np.zeros((nEvts,biggest_pCloud,4)) 
+
+    #===================#
+    ## FILL IN ENTRIES ##==============================================================
+    #===================#
+    for i in range(len(event_indices)):
+        # pull all relevant indices
+        evt = event_indices[i,0]
+        # recall this now returns an array
+        cluster_nums = event_indices[i,1]
+
+        ##############
+        ## CLUSTERS ##
+        ##############
+        # set up to have no clusters, further this with setting up the same thing for tracks
+        # find averaged center of clusters
+
+        cluster_Eta = event_dict['cluster_Eta'][evt].to_numpy()
+        cluster_Phi = event_dict['cluster_Phi'][evt].to_numpy()
+        av_Eta = np.mean(cluster_Eta)
+        av_Phi = np.mean(cluster_Phi)
+
+        nClust_current_total = 0
+        for c in cluster_nums:            
+            # cluster data
+            cluster_cell_ID = event_dict['cluster_cell_ID'][evt][c].to_numpy()
+            nInClust = len(cluster_cell_ID)
+            cluster_cell_E = event_dict['cluster_cell_E'][evt][c].to_numpy()    
+            cell_indices = dsu.find_index_1D(cluster_cell_ID, cell_ID_dict)
+
+            cluster_cell_Eta = geo_dict['cell_geo_eta'][cell_indices]
+            cluster_cell_Phi = geo_dict['cell_geo_phi'][cell_indices]
+            cluster_cell_rPerp = geo_dict['cell_geo_rPerp'][cell_indices]
+
+            # input all the data
+            # note here we leave the fourth entry zeros (zero for flag!!!)
+            low = nClust_current_total
+            high = low + nInClust
+            X_new[i,low:high,0] = cluster_cell_E
+            # Normalize to average cluster centers
+            X_new[i,low:high,1] = cluster_cell_Eta - av_Eta
+            X_new[i,low:high,2] = cluster_cell_Phi - av_Phi
+            X_new[i,low:high,3] = cluster_cell_rPerp
+
+            nClust_current_total += nInClust
+
+            #####################
+            ## TARGET ENERGIES ##
+            #####################
+            # this should be flattened or loaded as np array instead of zeroth index in future
+            Y_new[i,low:high,0] = event_dict['cluster_cell_hitsE_EM'][evt][c]
+            Y_new[i,low:high,1] = event_dict['cluster_cell_hitsE_nonEM'][evt][c]
+
+    #####################################################
+    t1 = t.time()
+    array_construction_time = t1 - t0
+    
+    
+    #=======================#
+    ## ARRAY CONCATENATION ##
+    #=======================#
+    t0 = t.time()
+    # Write to X
+    old_tot = tot_nEvts - nEvts
+    XR_large[old_tot:tot_nEvts, :max_dims[1], :4] = np.ndarray.copy(X_new)
+    # pad the remainder with zeros (just to be sure)
+    fill_shape_X = (nEvts, 1700 - max_dims[1], 4)
+    XR_large[old_tot:tot_nEvts, max_dims[1]:1700, :4] = np.zeros(fill_shape_X)
+    
+    # Write to Y
+    YR_large[old_tot:tot_nEvts, :max_dims[1], :2] = np.ndarray.copy(Y_new)
+    # pad the remainder with zeros (just to be sure)
+    fill_shape_Y = (nEvts, 1700 - max_dims[1], 2)
+    YR_large[old_tot:tot_nEvts, max_dims[1]:1700, :2] = np.zeros(fill_shape_Y)
+
+        
+    t1 = t.time()
+    time_to_memmap = t1-t0
+    
+    thisfile_t_tot = event_cuts_time+array_construction_time+time_to_memmap
+    t_tot += thisfile_t_tot
+    
+    print('Array dimension: '+str(max_dims))
+    print('Time to create dicts and select events: '+str(event_cuts_time))
+    print('Time to populate elements: '+str(array_construction_time))
+    print('Time to copy to memory map: '+str(time_to_memmap))
+    print('Time for this file: '+str(thisfile_t_tot))
+    print('Total events: '+str(tot_nEvts))
+    print('Current size: '+str((tot_nEvts,max_nPoints,4)))
+    print('Total time: '+str(t_tot))
+    print()
+
+t0 = t.time()
+X = np.lib.format.open_memmap('/data/atlas/rbate/Rho_X_'+str(Nfile)+'_files.npy',
+                             mode='w+', dtype=np.float64, shape=(tot_nEvts, max_nPoints, 4))
+np.copyto(dst=X, src=XR_large[:tot_nEvts,:max_nPoints,:], casting='same_kind', where=True)
+del XR_large
+os.system('rm /data/atlas/rbate/XR_large.npy')
+
+Y = np.lib.format.open_memmap('/data/atlas/rbate/Rho_Y_'+str(Nfile)+'_files.npy',
+                             mode='w+', dtype=np.float64, shape=(tot_nEvts, max_nPoints, 2))
+np.copyto(dst=Y, src=YR_large[:tot_nEvts,:max_nPoints,:], casting='same_kind', where=True)
+del YR_large
+os.system('rm /data/atlas/rbate/YR_large.npy')
+
+t1 = t.time()
+print()
+print('Time to copy new and delete old: '+str(t1-t0)+' (s)')
+print()       
+        
--- util/deep_set_util.py
+++ util/deep_set_util.py
@@ -4,6 +4,25 @@
 # Author: R. Bate - russellbate@phas.ubc.ca
 #===========================================================================================
 
+import numpy as np
+
+track_branches = ['trackEta_EMB1', 'trackPhi_EMB1', 'trackEta_EMB2', 'trackPhi_EMB2', 'trackEta_EMB3', 'trackPhi_EMB3',
+                  'trackEta_TileBar0', 'trackPhi_TileBar0', 'trackEta_TileBar1', 'trackPhi_TileBar1',
+                  'trackEta_TileBar2', 'trackPhi_TileBar2']
+
+event_branches = ["cluster_nCells", "cluster_cell_ID", "cluster_cell_E", 'cluster_nCells', "nCluster", "eventNumber",
+                  "nTrack", "nTruthPart", "truthPartPdgId", "cluster_Eta", "cluster_Phi", 'trackPt', 'trackP',
+                  'trackMass', 'trackEta', 'trackPhi', 'truthPartE', 'cluster_ENG_CALIB_TOT', "cluster_E", 'truthPartPt']
+
+ak_event_branches = ["cluster_nCells", "cluster_cell_ID", "cluster_cell_E", "cluster_nCells",
+                  "nTruthPart", "truthPartPdgId", "cluster_Eta", "cluster_Phi", "trackPt", "trackP",
+                  "trackMass", "trackEta", "trackPhi", "truthPartE", "cluster_ENG_CALIB_TOT", "cluster_E", "truthPartPt",
+                    "cluster_cell_hitsE_EM", "cluster_cell_hitsE_nonEM"]
+
+np_event_branches = ["nCluster", "eventNumber", "nTrack", "nTruthPart"]
+
+geo_branches = ["cell_geo_ID", "cell_geo_eta", "cell_geo_phi", "cell_geo_rPerp", "cell_geo_sampling"]
+
 def tvt_num(data, tvt=(75, 10, 15)):
     ''' Function designed to output appropriate numbers for traning validation and testing given
     a variable length input. TVT expressed as ratios and do not need to add to 100. '''
@@ -45,3 +64,53 @@ def dict_from_tree(tree, branches=None, np_branches=None):
         raise ValueError("No branches passed to function.")
         
     return dictionary
+
+def find_max_calo_cells(events, event_dict):
+    ''' This function is designed to work as the find_max_dim_tuple except we are negating tracks here '''
+    return None
+    
+def find_max_dim_tuple(events, event_dict):
+    ''' This function is designed to return the sizes of a numpy array such that we are efficient
+    with zero padding. Please feel free to write this faster, it can be done. Notes: we add six
+    to the maximum cluster number such that we have room for track info.
+    Inputs:
+        _events: filtered list of events to choose from in an Nx3 format for event, track, cluster index 
+        _event_tree: the event tree dictionary
+    Returns:
+        3-tuple consisting of (number of events, maximum cluster_size, 5), 5 because of how we have structured
+        the X data format in energyFlow to be Energy, Eta, Phi, rPerp, track flag, sampling layer
+        _cluster_ENG_CALIB_TOT, turthPartE '''    
+    nEvents = len(events)
+    max_clust = 0
+    
+    for i in range(nEvents):
+        event = events[i,0]
+        track_nums = events[i,1]
+        clust_nums = events[i,2]
+        
+        clust_num_total = 0
+        # set this to six for now to handle single track events, change later
+        track_num_total = 6
+        
+        # Check if there are clusters, None type object may be associated with it
+        if clust_nums is not None:
+            # Search through cluster indices
+            for clst_idx in clust_nums:
+                nInClust = len(event_dict['cluster_cell_ID'][event][clst_idx])
+                # add the number in each cluster to the total
+                clust_num_total += nInClust
+
+        total_size = clust_num_total + track_num_total
+        if total_size > max_clust:
+            max_clust = total_size
+    
+    # 6 for energy, eta, phi, rperp, track flag, sample layer
+    return (nEvents, max_clust, 6)
+
+def find_index_1D(values, dictionary):
+    ''' Use a for loop and a dictionary. values are the IDs to search for. dict must be in format 
+    (cell IDs: index) '''
+    idx_vec = np.zeros(len(values), dtype=np.int32)
+    for i in range(len(values)):
+        idx_vec[i] = dictionary[values[i]]
+    return idx_vec

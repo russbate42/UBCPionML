23bad65eb8ff5ea39189032feefcc307e8c8685a
russbate42 <r.bate@hotmail.com>
2021-10-29 18:43:21 -0700

    Training on PointNet with segmentation problem. Outputs are still incorrect.

--- util/deep_set_util.py
+++ util/deep_set_util.py
@@ -23,6 +23,45 @@ np_event_branches = ["nCluster", "eventNumber", "nTrack", "nTruthPart"]
 
 geo_branches = ["cell_geo_ID", "cell_geo_eta", "cell_geo_phi", "cell_geo_rPerp", "cell_geo_sampling"]
 
+cell_meta = {
+    'EMB1': {
+        'cell_size_phi': 0.098,
+        'cell_size_eta': 0.0031,
+        'len_phi': 4,
+        'len_eta': 128
+    },
+    'EMB2': {
+        'cell_size_phi': 0.0245,
+        'cell_size_eta': 0.025,
+        'len_phi': 16,
+        'len_eta': 16
+    },
+    'EMB3': {
+        'cell_size_phi': 0.0245,
+        'cell_size_eta': 0.05,
+        'len_phi': 16,
+        'len_eta': 8
+    },
+    'TileBar0': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.1,
+        'len_phi': 4,
+        'len_eta': 4
+    },
+    'TileBar1': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.1,
+        'len_phi': 4,
+        'len_eta': 4
+    },
+    'TileBar2': {
+        'cell_size_phi': 0.1,
+        'cell_size_eta': 0.2,
+        'len_phi': 4,
+        'len_eta': 2
+    },
+}
+
 def tvt_num(data, tvt=(75, 10, 15)):
     ''' Function designed to output appropriate numbers for traning validation and testing given
     a variable length input. TVT expressed as ratios and do not need to add to 100. '''
@@ -114,3 +153,105 @@ def find_index_1D(values, dictionary):
     for i in range(len(values)):
         idx_vec[i] = dictionary[values[i]]
     return idx_vec
+
+def to_xyz(coords):
+    ''' Simple geometric conversion to xyz from eta, phi, rperp (READ: in this order)
+    Inputs: np array of shape (N, 3) where columns are [eta, phi, rPerp]
+    Outputs: np array of shape (N, 3) where columns are [x,y,z] '''
+    if len(coords.shape) == 2:
+        eta = coords[:,0]
+        phi = coords[:,1]
+        rperp = coords[:,2]
+        theta = 2*np.arctan( np.exp(-eta) )
+    
+        cell_x = rperp*np.cos(phi)
+        cell_y = rperp*np.sin(phi)
+        cell_z = rperp/np.tan(theta)
+    
+        return np.column_stack([cell_x,cell_y,cell_z])
+
+    elif len(coords.shape) == 3:
+        
+        eta = coords[:,:,0]
+        phi = coords[:,:,1]
+        rperp = coords[:,:,2]
+        theta = 2*np.arctan( np.exp(-eta) )
+    
+        cell_x = rperp*np.cos(phi)
+        cell_y = rperp*np.sin(phi)
+        cell_z = rperp/np.tan(theta)
+        
+        return np.stack(cell_x,cell_y,cell_z, axis=2)
+    
+    else:
+        raise ValueError('Unsupported array type in to_xyz()')
+        
+def weighted_sum(variable, weights):
+    ''' Discrete first moment. '''
+    Ans = np.dot(weights, variable)
+    Ans = Ans / np.sum(weights)
+    return Ans
+    
+def find_centroid(coords_3d, targets):
+    ''' Designed to find the energy weighted centroid of all the cells in the cluster.
+    Inputs:
+        X: raw x, y, z without masking 
+        Y: energies in order of EM, nonEM'''
+    
+    xmask = coords_3d[:,0] != 0
+    ymask = coords_3d[:,1] != 0
+    mask = np.logical_or(xmask, ymask)
+
+    EM_weights = targets[mask,0]
+    nonEM_weights = targets[mask,1]
+    
+    EM_centr = weighted_sum(variable=coords_3d[mask,:3], weights=EM_weights)
+    nonEM_centr = weighted_sum(variable=coords_3d[mask,:3], weights=nonEM_weights)
+
+    return EM_centr, nonEM_centr
+
+
+def create_image(cell_size_eta, cell_size_phi, nEta, nPhi, layer_eta, layer_phi, layer_E, mask,
+                 eta_shift=0, phi_shift=0, logscale=True):
+    ''' Takes layer and cluster information to produce a pixel image using binning.
+    Inputs: 
+        cell_size_eta/phi: size of each calo cell for that layer
+        nEta/nPhi: discrete number of points which to include
+        later_eta/phi/E: the input values of the cluster
+        mask: which values to choose from
+        eta/phi_shift: artificially move the cluster in the eta phi plane
+        logscale: whether or not to take the log of the energy values
+    Outputs:
+        (n,m) array with energy values filled in the appropriate bins, otherwise np.nan
+    '''
+    
+    eta_span = cell_size_eta * nEta
+    phi_span = cell_size_phi * nPhi
+    eta_start = - int(nEta/2) * cell_size_eta
+    phi_start = - int(nPhi/2) * cell_size_phi
+    
+    # fill with np.nan so hopefully imshow wont plot this?
+    image = np.full((nEta, nPhi), np.nan)
+    point_out = 0
+    
+    for i in range(np.count_nonzero(mask)):
+        
+        N = int(np.floor( ( (layer_eta[i] - eta_start)/eta_span) * nEta ))
+        M = int(np.floor( ( (layer_phi[i] - phi_start)/phi_span) * nPhi ))
+        
+        inbounds = True
+        if N < 0 or M < 0:
+            inbounds = False
+        if N >= nEta or M >= nPhi:
+            inbounds = False
+            
+        if inbounds == True:
+            if logscale:
+                image[N,M] = np.log(layer_E[i])
+            else:
+                image[N,M] = layer_E[i]
+        else:
+            point_out += 1
+            
+    print(str(point_out)+' points out of bounds')
+    return image

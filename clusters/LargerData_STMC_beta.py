Revised STMCv2 - using EMx2 for centering. Fixed bug with negative rPerp. Changed normalization script to use centered target energy. Wrote script to split tracks and clusters from normalized results to train against truth particle energy. Write script to investigate eneryg distribution for dR cuts - STMC_beta. Expanded dR cuts for STMCv2. Added argument to support plot utils and legend font size. train_pflow.py now has support for adding save strings, and choosing data type.

clusters/LargerData_STMC_beta.py
@@ -4,10 +4,8 @@ Author: Russell Bate
 russell.bate@cern.ch
 russellbate@phas.ubc.ca
 
-Notes: Version 2 of the STMC data script.
-- single tracks
-- clusters within DeltaR of 1.2 of track
-- energy weighted cluster average '''
+Notes: This script (beta) is designed as a test for the energy
+regime of the tracks '''
 #====================
 # Load Utils ========
 #====================
@@ -70,7 +68,7 @@ def find_max_dim_tuple(events, event_dict):
         
         clust_num_total = 0
         # set this to six for now to handle single track events, change later
-        track_num_total = 10 # max 9 but keep a buffer of 1
+        track_num_total = 10 # I think its seven but keep a buffer
         
         # Check if there are clusters, None type object may be associated with it
         if clust_nums is not None:
@@ -116,6 +114,10 @@ def find_index_1D(values, dictionary):
 #====================
 # Metadata ==========
 #====================
+track_branches = ['trackEta_EMB1', 'trackPhi_EMB1', 'trackEta_EMB2', 'trackPhi_EMB2', 'trackEta_EMB3', 'trackPhi_EMB3',
+                  'trackEta_TileBar0', 'trackPhi_TileBar0', 'trackEta_TileBar1', 'trackPhi_TileBar1',
+                  'trackEta_TileBar2', 'trackPhi_TileBar2']
+
 event_branches = ["cluster_nCells", "cluster_cell_ID", "cluster_cell_E", 'cluster_nCells', "nCluster", "eventNumber",
                   "nTrack", "nTruthPart", "truthPartPdgId", "cluster_Eta", "cluster_Phi", 'trackPt', 'trackP',
                   'trackMass', 'trackEta', 'trackPhi', 'truthPartE', 'cluster_ENG_CALIB_TOT', "cluster_E", 'truthPartPt']
@@ -132,9 +134,6 @@ geo_branches = ["cell_geo_ID", "cell_geo_eta", "cell_geo_phi", "cell_geo_rPerp",
 #======================================
 # Track related meta-data
 #======================================
-trk_em_eta = ['trackEta_EMB2', 'trackEta_EME2']
-trk_em_phi = ['trackPhi_EMB2', 'trackPhi_EME2']
-
 trk_proj_eta = ['trackEta_EMB1', 'trackEta_EMB2', 'trackEta_EMB3',
     'trackEta_EME1', 'trackEta_EME2', 'trackEta_EME3', 'trackEta_HEC0',
     'trackEta_HEC1', 'trackEta_HEC2', 'trackEta_HEC3', 'trackEta_TileBar0',
@@ -189,27 +188,33 @@ geo_dict = dict_from_tree(tree=CellGeo_tree, branches=None, np_branches=geo_bran
 cell_geo_ID = geo_dict['cell_geo_ID']
 cell_ID_dict = dict(zip(cell_geo_ID, np.arange(len(cell_geo_ID))))
 
+# additional geometry data
+# This should be deleted if new track projection technique works.
+layer_rPerp = np.array([1540., 1733., 1930., 2450., 3010., 3630.])
+track_sample_layer = np.array([1,2,3,12,13,14])
+
+# for event dictionary
+events_prefix = '/fast_scratch_1/atlas_images/v01-45/pipm/'
+
+# Use this to compare with the dimensionality of new events
+firstArray = True
+
 ## MEMORY MAPPED ARRAY ALLOCATION ##
 X_large = np.lib.format.open_memmap('/data/atlas/rbate/X_large.npy', mode='w+', dtype=np.float64,
                        shape=(2500000,1500,6), fortran_order=False, version=None)
 Y_large = np.lib.format.open_memmap('/data/atlas/rbate/Y_large.npy', mode='w+', dtype=np.float64,
                        shape=(2500000,3), fortran_order=False, version=None)
+TPE_STAC = np.empty(2500000)
+TPE_STMC = np.empty(2500000)
 Eta_large = np.empty(2500000)
 
-
-# Pre-Loop Definitions ##
-#======================================
 k = 1 # tally used to keep track of file number
 tot_nEvts = 0 # used for keeping track of total number of events
+tot_nEvts2 = 0 # Used for tracking track only filter
 max_nPoints = 0 # used for keeping track of the largest 'point cloud'
 t_tot = 0 # total time
-# for event dictionary
-events_prefix = '/fast_scratch_1/atlas_images/v01-45/pipm/'
 num_zero_tracks = 0
 
-
-## Main File Loop ##
-#======================================
 for currFile in fileNames:
     
     # Check for file, a few are missing
@@ -245,6 +250,7 @@ for currFile in fileNames:
     # SINGLE TRACK CUT
     single_track_mask = event_dict['nTrack'] == np.full(nEvents, 1)
     single_track_filter = all_events[single_track_mask]
+
     
     # TRACKS WITH CLUSTERS
     nCluster = event_dict['nCluster'][single_track_filter]
@@ -252,6 +258,22 @@ for currFile in fileNames:
     filtered_event = single_track_filter[nz_clust_mask]
     t1 = t.time()
     events_cuts_time = t1 - t0
+    
+    
+    #==================================##
+    ## CREATE all cluster truth energy ##
+    #===================================#
+    t0 = t.time()
+    tracks_thisfile = single_track_filter.shape[0]
+    TPE_all = np.zeros(tracks_thisfile)
+    for i, evt in enumerate(single_track_filter):
+        TPE_all[i] = event_dict['truthPartE'][evt][0]
+        
+    tot_nEvts2 += tracks_thisfile
+    TPE_STAC[tot_nEvts2-tracks_thisfile:tot_nEvts2] = np.ndarray.copy(TPE_all)
+    t1 = t.time()
+    extra_time = t1 - t0
+    
     
     #============================================#
     ## CREATE INDEX ARRAY FOR TRACKS + CLUSTERS ##
@@ -277,7 +299,7 @@ for currFile in fileNames:
                                    event_dict["cluster_Phi"][evt].to_numpy()), axis=1)
 
         _DeltaR = DeltaR(clusterCoords, trackCoords)
-        DeltaR_mask = _DeltaR < 1.2
+        DeltaR_mask = _DeltaR < .2
         matched_clusters = cluster_idx[DeltaR_mask]
 
         ## CREATE LIST ##
@@ -301,6 +323,7 @@ for currFile in fileNames:
         max_nPoints = max_dims[1]
     
     # Create arrays
+    TPE_stmc = np.zeros(max_dims[0])
     Y_new = np.zeros((max_dims[0],3))
     X_new = np.zeros(max_dims)
     Eta_new = np.zeros(max_dims[0])
@@ -318,34 +341,6 @@ for currFile in fileNames:
         # recall this now returns an array
         cluster_nums = event_indices[i,2]
 
-        ## Centering ##
-        trk_bool_em = np.zeros(2, dtype=bool)
-        trk_full_em = np.empty((2,2))
-    
-        for l, (eta_key, phi_key) in enumerate(zip(trk_em_eta, trk_em_phi)):
-
-            eta_em = track_dict[eta_key][evt][track_idx]
-            phi_em = track_dict[phi_key][evt][track_idx]
-
-            if np.abs(eta_em) < 2.5 and np.abs(phi_em) <= np.pi:
-                trk_bool_em[l] = True
-                trk_full_em[l,0] = eta_em
-                trk_full_em[l,1] = phi_em
-                
-        nProj_em = np.count_nonzero(trk_bool_em)
-        if nProj_em == 1:
-            eta_ctr = trk_full_em[trk_bool_em, 0]
-            phi_ctr = trk_full_em[trk_bool_em, 1]
-            
-        elif nProj_em == 2:
-            trk_av_em = np.mean(trk_full_em, axis=1)
-            eta_ctr = trk_av_em[0]
-            phi_ctr = trk_av_em[1]
-            
-        elif nProj_em == 0:
-            eta_ctr = event_dict['trackEta'][evt][track_idx]
-            phi_ctr = event_dict['trackPhi'][evt][track_idx]      
-        
         ##############
         ## CLUSTERS ##
         ##############
@@ -358,6 +353,10 @@ for currFile in fileNames:
             cluster_Phi = event_dict['cluster_Phi'][evt].to_numpy()
             cluster_E = event_dict['cluster_E'][evt].to_numpy()
             cl_E_tot = np.sum(cluster_E)
+            
+            # Energy weighted average
+            av_Eta = np.dot(cluster_Eta, cluster_E)/cl_E_tot
+            av_Phi = np.dot(cluster_Phi, cluster_E)/cl_E_tot
 
             nClust_current_total = 0
             target_ENG_CALIB_TOT = 0
@@ -380,8 +379,8 @@ for currFile in fileNames:
                 high = low + nInClust
                 X_new[i,low:high,0] = cluster_cell_E
                 # Normalize to average cluster centers
-                X_new[i,low:high,1] = cluster_cell_Eta - eta_ctr
-                X_new[i,low:high,2] = cluster_cell_Phi - eta_ctr
+                X_new[i,low:high,1] = cluster_cell_Eta - av_Eta
+                X_new[i,low:high,2] = cluster_cell_Phi - av_Phi
                 X_new[i,low:high,3] = cluster_cell_rPerp
                 X_new[i,low:high,5] = cluster_cell_sampling
 
@@ -394,6 +393,7 @@ for currFile in fileNames:
         Y_new[i,0] = event_dict['truthPartE'][evt][0]
         Y_new[i,1] = event_dict['truthPartPt'][evt][track_idx]
         Y_new[i,2] = target_ENG_CALIB_TOT
+        TPE_stmc[i] = event_dict['truthPartE'][evt][0]
         
         #########
         ## ETA ##
@@ -427,20 +427,10 @@ for currFile in fileNames:
                     
                 elif cnum in fixed_z_numbers:
                     z = z_calo_dict[cnum]
-                    aeta = np.abs(eta)
-                    rPerp = z*2*np.exp(aeta)/(np.exp(2*aeta) - 1)
+                    rPerp = z*2*np.exp(eta)/(np.exp(2*eta) - 1)
                     
                 else:
                     raise ValueError('Calo sample num not found in dicts..')
-                
-                if rPerp < 0:
-                    print()
-                    print('Found negative rPerp'); print()
-                    print('Event number: {}'.format(evt))
-                    print('Eta: {}'.format(eta))
-                    print('Phi: {}'.format(phi))
-                    print('rPerp: {}'.format(rPerp))
-                    raise ValueError('Found negative rPerp')
                     
                 trk_full[j,2] = rPerp
                 
@@ -452,8 +442,8 @@ for currFile in fileNames:
             trk_arr = np.empty((1, 6))
             num_zero_tracks += 1
             trk_arr[:,0] = event_dict['trackP'][evt][track_idx]
-            trk_arr[:,1] = event_dict['trackEta'][evt][track_idx] - eta_ctr
-            trk_arr[:,2] = event_dict['trackPhi'][evt][track_idx] - phi_ctr
+            trk_arr[:,1] = event_dict['trackEta'][evt][track_idx] - av_Eta
+            trk_arr[:,2] = event_dict['trackPhi'][evt][track_idx] - av_Phi
             trk_arr[:,3] = 1532.18 # just place it in EMB1
             trk_arr[:,4] = 1 # track flag
             trk_arr[:,5] = 1 # place layer in EMB1
@@ -465,8 +455,8 @@ for currFile in fileNames:
             trk_arr[:,5] = np.ndarray.copy(trk_full[trk_bool,3])
             trk_arr[:,0] = trackP/trk_proj_num
 
-            trk_arr[:,1] = trk_arr[:,1] - eta_ctr
-            trk_arr[:,2] = trk_arr[:,2] - phi_ctr
+            trk_arr[:,1] = trk_arr[:,1] - av_Eta
+            trk_arr[:,2] = trk_arr[:,2] - av_Phi
 
         X_new[i,high:high+trk_proj_num,:] = np.ndarray.copy(trk_arr)
     
@@ -490,6 +480,9 @@ for currFile in fileNames:
     
     # Eta
     Eta_large[old_tot:tot_nEvts] = np.ndarray.copy(Eta_new)
+    
+    # Energy
+    TPE_STMC[old_tot:tot_nEvts] = np.ndarray.copy(TPE_stmc)
         
     t1 = t.time()
     time_to_memmap = t1-t0
@@ -525,19 +518,9 @@ os.system('rm /data/atlas/rbate/Y_large.npy')
 
 np.save('/data/atlas/rbate/Eta_STMC_v2_'+str(Nfile)+'_files', Eta_large[:tot_nEvts])
 
+np.savez('/data/atlas/rbate/STMC_v2_Energy_'+str(Nfile)+'_files', args=(TPE_STAC[:tot_nEvts], TPE_STMC[:tot_nEvts]), kwds=('STAC', 'STMC'))
 
 t1 = t.time()
 print()
 print('Time to copy new and delete old: '+str(t1-t0)+' (s)')
 print()
-
-# t0 = t.time()
-# np.savez('/data/rbate/XY_STMC_allFiles', X, Y)
-# t1 = t.time()
-# print()
-# print('Time to save file: '+str(t1-t0)+' (s)')
-# print()
-        
-    
-
-

regression/normalize_STMC.py
@@ -15,11 +15,12 @@ print()
 print('starting data normalization for STMC..v2..');print()
 
 ## General Imports
-#======================================
+#===============w=======================
 import numpy as np
 import time
 from time import process_time as cput
 import os
+cwd = os.getcwd()
 
 ## Local ML Packages
 import sys
@@ -50,7 +51,12 @@ print('Time to load memory mapped data and copy: {:8.6f} (s)'.format(t1-t0))
 print()
 print('normalizing target..')
 t0 = cput()
-Y = np.log(Yraw[:,0])
+Y_log_mean = np.mean(np.log(Yraw[:,0]))
+print('Y_log_mean: ')
+print(Y_log_mean); print()
+with open(cwd+'/STMC_v2_25_files_Y_logmean.txt', 'w+') as f:
+    f.write('{}'.format(Y_log_mean))
+Y = np.log(Yraw[:,0]) - Y_log_mean
 t1 = cput()
 print('{:6.2f} (m)'.format((t1-t0)/60));print()
 print('normalized target..');print()
@@ -88,6 +94,10 @@ X[nz_mask,1] = np.ndarray.copy(Xraw[nz_mask,1]/eta_std)
 # phi_mask = X[:,:,2] != 0
 cellPhi_std = np.std(Xraw[nz_mask,2])
 X[nz_mask,2] = np.ndarray.copy(Xraw[nz_mask,2]/cellPhi_std)
+
+# Track Flag!
+X[nz_mask,4] = np.ndarray.copy(Xraw[nz_mask,4])
+
 t1 = cput()
 print('Time to Normalize: {:6.2f} (m)'.format((t1-t0)/60))
 print()
@@ -105,18 +115,18 @@ shuffle_time = t1 - t0
 
 print('Saving shuffled data..')
 t0 = cput()
-Xfin = np.lib.format.open_memmap('/fast_scratch_1/atlas/X_STMC_v2_25_norm.npy',
+Xfin = np.lib.format.open_memmap('/fast_scratch_1/atlas/X_STMC_v2_25_norm2.npy',
                              mode='w+', dtype=np.float64, shape=(Xraw.shape[0],
                                                          Xraw.shape[1], 5))
 
-Yfin = np.lib.format.open_memmap('/fast_scratch_1/atlas/Y_STMC_v2_25_norm.npy',
+Yfin = np.lib.format.open_memmap('/fast_scratch_1/atlas/Y_STMC_v2_25_norm2.npy',
                              mode='w+', dtype=np.float64, shape=(Yraw.shape[0],))
 
 np.copyto(src=X[indices,:,:], dst=Xfin, casting='same_kind', where=True)
 np.copyto(src=Y[indices], dst=Yfin, casting='same_kind', where=True)
 
 EtaFin = np.ndarray.copy(EtaRaw[indices])
-np.save('/fast_scratch_1/atlas/Eta_STMC_v2_25_norm', EtaFin)
+np.save('/fast_scratch_1/atlas/Eta_STMC_v2_25_norm2', EtaFin)
 
 del X
 del Y
@@ -128,8 +138,8 @@ save_time = t1 - t0
 print()
 print('time to copy shuffled files: {:8.2f} (m)'.format((t1-t0)/60))
 print()
-print('Total time: {:8.2f}'.format(load_time+target_time+zero_elem_time\
-                         +input_time+shuffle_time+save_time))
+print('Total time: {:8.2f} (m)'.format((load_time+target_time+zero_elem_time\
+                         +input_time+shuffle_time+save_time)/60))
 print()
 print('finished normalizing the STMC v2 dataset')
 print()

regression/train_pflow.py
@@ -5,6 +5,7 @@
 
 author: Russell Bate
 russellbate@phas.ubc.ca
+russell.bate@cern.ch
 '''
 
 ## META-DATA ##
@@ -63,6 +64,10 @@ parser.add_argument('--graph_execution', action="store_true",
                     dest="graph_execution")
 parser.add_argument('--save_model', action="store_true", dest="save_model")
 parser.add_argument('--save_results', action="store_true", dest="save_results")
+parser.add_argument('--data', action="store", dest='datafiles',
+                    default='CL+TR', type=str)
+parser.add_argument('--save_tag', action="store", dest='savetag',
+                    default=None, type=str)
 
 args = parser.parse_args()
 
@@ -114,7 +119,38 @@ else:
         print('Training on {} events'.format(NEVENTS))
         print('Eager or graph? -- {}'.format(exec_str))
 
-        
+
+## Choose which files to train from!
+datafiles = args.datafiles
+# the following three are from the STMC_v2 dataset
+if datafiles == 'CL+TR':
+    Xfile = 'X_STMC_v2_25_norm2.npy'
+    Yfile = 'Y_STMC_v2_25_norm2.npy'
+    Etafile = 'Eta_STMC_v2_25_norm2.npy'
+elif datafiles == 'CL-TR':
+    Xfile = 'X_STMC_v2_Cl_25.npy'
+    Yfile = 'Y_STMC_v2_25_norm2.npy'
+    Etafile = 'Eta_STMC_v2_25_norm2.npy'
+elif datafiles == 'TR-CL':
+    Xfile = 'X_STMC_v2_Tr_25.npy'
+    Yfile = 'Y_STMC_v2_25_norm2.npy'
+    Etafile = 'Eta_STMC_v2_25_norm2.npy'
+# this is a new dataset with Cluster Only
+elif datafiles == 'CLO':
+    Xfile = ''
+    Yfile = ''
+    Etafile = ''
+    sys.exit('Cluster Only cluster_ENG_CALIB_TOT not ready yet.\n')
+else:
+    raise ValueError('incorrect option passed to --data')
+
+## Add information to the savestring!
+if args.savetag is None:
+    extra_info = ''
+else:
+    extra_info = '_'+args.savetag
+
+
 ## TF Environment
 #======================================
 import tensorflow as tf
@@ -139,15 +175,15 @@ print()
 print('Loading data..')
 t0 = cput()
 if args.full_data:
-    X = np.load(datapath_prefix+'X_STMC_v2_25_norm.npy', mmap_mode='r+')
-    Y = np.load(datapath_prefix+'Y_STMC_v2_25_norm.npy', mmap_mode='r+')
-    E = np.load(datapath_prefix+'Eta_STMC_v2_25_norm.npy', mmap_mode='r+')
+    X = np.load(datapath_prefix+Xfile, mmap_mode='r+')
+    Y = np.load(datapath_prefix+Yfile, mmap_mode='r+')
+    E = np.load(datapath_prefix+Etafile, mmap_mode='r+')
 else:
-    X = np.load(datapath_prefix+'X_STMC_v2_25_norm.npy', mmap_mode='r+')\
+    X = np.load(datapath_prefix+Xfile, mmap_mode='r+')\
             [:NEVENTS,:,:]
-    Y = np.load(datapath_prefix+'Y_STMC_v2_25_norm.npy', mmap_mode='r+')\
+    Y = np.load(datapath_prefix+Yfile, mmap_mode='r+')\
             [:NEVENTS]
-    E = np.load(datapath_prefix+'Eta_STMC_v2_25_norm.npy', mmap_mode='r+')\
+    E = np.load(datapath_prefix+Etafile, mmap_mode='r+')\
             [:NEVENTS]
 t1 = cput()
 print('time to load data: {:6.4f} (s)'.format(t1-t0)); print()
@@ -207,11 +243,9 @@ elif MODEL == 'PFN_wDropout':
     model = PFN_wDropout(num_points=X.shape[1], num_features=X.shape[2],
                      name=MODEL)
 elif MODEL == 'PFN_wAttention':
-    print('Not built yet saaaarrryy :) ..')
-    sys.exit()
+    sys.exit('Not built yet saaaarrryy :) ..')
 else:
-    print('Unknown model. Quitting program.')
-    sys.exit()
+    sys.exit('Unknown model. Quitting program.')
 
 model.compile(loss='mse', optimizer=keras.optimizers.Adam(
     learning_rate=LEARNING_RATE))
@@ -263,17 +297,18 @@ print()
 
 ## Save Information
 #======================================
-infostring = '{}_STMC--LR_{:.0e}--BS_{}--EP_{}--EV_{}--{}'.format(MODEL,
-                LEARNING_RATE, BATCH_SIZE, EPOCHS, NEVENTS, DATE)
+infostring = '{}_STMCv2--LR_{:.0e}--BS_{}--EP_{}--EV_{}--{}{}'.format(MODEL,
+                LEARNING_RATE, BATCH_SIZE, EPOCHS, NEVENTS, DATE,
+                extra_info)
 if SAVE_RESULTS == True:
-    print('saving_files..')
+    print('saving results..');print()
     with open('results/history_'+infostring+'.pickle', 'wb')\
              as histfile:
         pickle.dump(history.history, histfile)
 
     np.savez('results/target_preds_'+infostring,
-            args=(np.exp(Y_test), np.exp(np.squeeze(prediction,
-            axis=1)), E_test), kwds=('target', 'prediction', 'Eta'))
+            args=(Y_test, np.squeeze(prediction,
+            axis=1), E_test), kwds=('target', 'prediction', 'Eta'))
 else:
     print('No saved results flag. No results will be saved.')
 

util/resolution_util.py
@@ -77,7 +77,8 @@ def choose_res(x, y, bins, statistic):
 def resolutionPlot(x, y, figfile='', statistic='std',
                    xlabel='Cluster Calib Hits', ylabel='Energy IQR over Median',
                    atlas_x=-1, atlas_y=-1, simulation=False,
-                   textlist=[], colors=None, labels=None):
+                   textlist=[], colors=None, labels=None,
+                   leg_font_size=12):
     
     # determine ranges for scale on plot
     xbin = [10**exp for exp in  np.arange(-1.0, 3.1, 0.1)]
@@ -109,7 +110,7 @@ def resolutionPlot(x, y, figfile='', statistic='std',
     pu.drawLabels(fig, atlas_x, atlas_y, simulation, textlist)
     
     if labels is not None:
-        plt.legend(loc='upper right')
+        plt.legend(loc='upper right', prop={'size':leg_font_size})
 
     if figfile != '':
         plt.savefig(figfile)